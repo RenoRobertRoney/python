4d7c3a775b8547053458087846bc91d6a0724b1d
Reno Robert Roney <renorobert777@gmail.com>
2025-07-10 14:57:50 +0530

    Add files via upload

---
+++ celTofeht,kelvin.py
@@ -0,0 +1,8 @@
+#
+celcius=float(input("Enter the temperature in celcius: "))
+farenheit=(celcius*9/5)+32
+kelvin=celcius+273.15
+print("Temperature in farenheit is:", farenheit)
+print("Temperature in kelvin is:", kelvin)
+print("Temperature in celcius is:", celcius)
+         
---
+++ name,age,dob.py
@@ -0,0 +1,6 @@
+name = input("Enter the name: ")
+year = int(input("Enter the year of birth: "))
+current_year = int(input("Enter the current year: "))
+current_age = current_year - year
+print("Name is:", name)
+print("Current age is:", current_age)
