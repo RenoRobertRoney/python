70209e4e297fbf22447c64c37ce6414227a2643e
Reno Robert Roney <renorobert777@gmail.com>
2025-07-17 14:37:28 +0530

    Add files via upload

---
+++ largest&smallest.py
@@ -0,0 +1,9 @@
+#write a program that uses built-in functions to find the largest and smallest among three numbers
+#Author: Reno Robert Roney
+#Rollno: 32
+a=list(map(int, input("Enter the list: ").split()))
+largest = max(a)
+smallest = min(a)
+print("Largest number is:", largest)
+print("Smallest number is:", smallest)
+
